f093884e5b812c02fab83aa71cfe9f6930190b85
AHmims <ali.hmims99@gmail.com>
2020-03-12 20:56:12 +0100

    add class Client

---
+++ app/model/dataObjects.js
@@ -0,0 +1,30 @@
+class Client {
+    constructor() {
+        this.cin = cin;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateN = dateNaissance;
+        this.email = email;
+        this.numeroTel = numeroTelephone;
+        this.motPass = motDePass;
+    }
+    // 
+    // RETURN ATTRIBUTES DATA AS AN ARRAY
+    getAll() {
+        return [this.cin, this.nom, this.prenom, this.dateN, this.email, this.numeroTel, this.motPass];
+    }
+    //RETUEN CREDENTIELS DATA
+    getCred(type = 'object') {
+        let returnData = {
+            id: this.email,
+            password: this.motPass
+        }
+        // 
+        if (type != 'object')
+            returnData = [this.email, this.motPass];
+        // 
+        return returnData;
+    }
+}
+// 
+class
