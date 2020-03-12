2824b3b6dd3c09459599f25bd55c6bbc86b92b29
AHmims <ali.hmims99@gmail.com>
2020-03-12 17:00:39 +0100

    fixed cors, better hints

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const _PORT = 8080;
 // 
 // SEND AN HTML FILE TO THE LOCAL SERVER ROOT
 _APP.get('/', (req, res) => {
-    res.sendFile(_PATH.join(__dirname, './app/index.html'));
+    res.sendFile(_PATH.join(__dirname, 'app/index.html'));
 });
+// GIVE THE LOCAL SERER TO ACCESS /APP FOLDER
+_APP.use('/', _EXPRESS.static(_PATH.join(__dirname, 'app')));
 // START THE SERVER
-_APP.listen(_PORT);
+_APP.listen(_PORT, () => {
+    console.log(`Listening on port ${_PORT}\nPlease refere to : localhost:${_PORT}`);
+});
