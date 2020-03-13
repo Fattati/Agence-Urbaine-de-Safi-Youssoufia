400cbcbf0141e5a5a2a7d6779bf9771a48c97d73
AHmims <ali.hmims99@gmail.com>
2020-03-13 14:56:15 +0100

    Save a class instence in a JSON file

--- app/model/dataStorage.js
+++ app/model/dataStorage.js
@@ -1,6 +1,7 @@
 // THE TYPE OF THE DATA THAT SHALL BE SAVED IN THE JSON FILES SHOULD BE CLASSES
 // AS FOR ABOVE, RETRIEVED DATA SHALL BE IN AN ARRAY OF CLASSES
 // 
+// TO-DO : ADD A FUNCTION THAT RETURNS ALL QUESTION IN DATE INTERVAL
 const _FS = require('fs-extra');
 const _PATH = require('path');
 const dataObjects = require('./dataObjects');
@@ -12,6 +13,7 @@ async function jsonGetAll(className) {
         // ARRAY THAT WILL STORE THE CLASSES
         let classArray = [];
         // 
+        // console.log(jsonDataObject);
         jsonDataObject.forEach(element => {
             classArray.push(jsonToClass(element), className);
         });
@@ -51,6 +53,35 @@ function searchBy(className, id) {
     // 
     return retValue;
 }
+// SAVE DATA INTO A JSON FILE
+async function jsonSave(className, data) {
+    const FILE_PATH = _PATH.join(__dirname, '..', 'data', `${className}.json`);
+
+
+    // SEE IF THE FILE EXISTS, IF NOT MAKE IT
+    if (!await _FS.pathExists(FILE_PATH))
+        await _FS.createFile(FILE_PATH);
+    // FILL THE JSON FILL IF EMPTY
+    let fileContent = await _FS.readFile(FILE_PATH, 'utf8');
+    if (fileContent.length == 0)
+        await _FS.writeFile(FILE_PATH, '[]');
+
+    // RETURN VALUE
+    let succes = true;
+    try {
+        // IN ORDER TO APPEND TO A JSON FILE I NEED TO READ THE EXISTING FILE
+        let jsonDataObject = JSON.parse(await _FS.readFile(FILE_PATH));
+        //THE ADD THE WENTED DATA TO IT
+        jsonDataObject.push(data.getAll());
+        //AFTER THAT I RESAVE THE JSON FILE
+        await _FS.writeJSON(FILE_PATH, jsonDataObject);
+    } catch (err) {
+        console.log(err);
+        succes = false;
+    }
+    // 
+    return succes;
+}
 // FUNCTION THAT RETURNS THE SERVER'S CURRENT DATE 🙌
 function getCurrentDate() {
     let date = new Date();
@@ -82,3 +113,10 @@ function jsonToClass(objectData, className) {
 async function pathExists(path) {
     return await _FS.pathExists(path);
 }
+// A FUNCTION TO CHECK WETHER THE FILE IS EMPTY, IF TRUE FILL IT WITH []
+async function initJsonFile(filePath) {
+    let fileContent = await _FS.readFile(filePath, 'utf8');
+    if (fileContent.length == 0)
+        _FS.writeJSON(filePath, []);
+}
+// 
